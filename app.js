432fb44151368abf83b778d239efbe0127b992cd
Stephane_tcha <stephanetcha@gmail.com>
2023-01-14 20:08:54 +0100

    ajout des events keydow et click, mise en place de la fonction calculer

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const touches = [...document.querySelectorAll('.bouton')];
 const listeKeycode = touches.map(touche => touche.dataset.key);
-console.log(listeKeycode);
+
+document.addEventListener('keydown', (e) => {
+    const valeur = e.keyboardEvent.code.toString();
+    calculer(valeur)
+})
+
+document.addEventListener('click', (e) => {
+    const valeur = e.target.dataset.key ;
+    calucler(valeur)
+})
+
+const calucler = (valeur) => {
+    if(listeKeycode.includes(valeur)){
+        
+    }
+}
